00734b4a5eab22992563770c6d5774f29f561796
EU1182053 <65112935+EU1182053@users.noreply.github.com>
2020-10-13 19:48:48 +0530

    Add files via upload

---
+++ client/src/admin/ManageCategory.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+import Base from "../core/Base";
+export default function ManageCategory() {
+  return (
+    <Base title="Manage Categories" description="You can perform actions like update,delete etc.">
+      <div>Manage Categories</div>
+    </Base>
+  );
+}

a3aa4a0b3d0000c8d5c8c0de5de0b5b5828c8aa5
SIDDHESH DATTA SAWANT <65112935+EU1182053@users.noreply.github.com>
2020-11-18 14:28:27 +0530

    Update coreapicalls.js
    sending data to the back-end.

--- client/src/core/helper/coreapicalls.js
+++ client/src/core/helper/coreapicalls.js
@@ -7,3 +7,18 @@ export const getProducts = () => {
     })
     .catch(err => console.log(err))
 }
+export const createReview = (userId, token, data) => {
+    return fetch(`${API}/review/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => console.log(err));
+  };

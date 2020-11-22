aca7cfc30fcaf2ce5fe3c61e3ae45a001bb8e025
SIDDHESH DATTA SAWANT <65112935+EU1182053@users.noreply.github.com>
2020-11-22 23:11:12 +0530

    Update product.js
    code for update the product.

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -117,3 +117,11 @@ exports.updateStock = (req, res, next) => {
     next();
   });
 };
+exports.updateProduct = () => {
+  Product.findByIdAndUpdate({_id: req.product._id},{$set: req.body},{new: true}).then(product => {
+    return res.status(200).json(product)
+  })
+  .catch(err => {
+    return res.json(err)
+  })
+}

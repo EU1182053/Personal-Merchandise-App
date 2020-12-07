5be806b044b0404bbd08c977ac94d4bd69957ba0
siddhesh <siddheshs871@gmail.com>
2020-12-07 20:54:05 +0530

    duplicate directory deleted

--- client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from 'react-dom'
-// import '../src/style.css'
-import Routes from './Routes'
-
-ReactDOM.render(<Routes/>, document.getElementById('root'));
-

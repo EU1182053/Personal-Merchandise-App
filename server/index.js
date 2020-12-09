5e2f887584d68340ffd1abcadaa08ea5aad38c64
siddhesh <siddheshs871@gmail.com>
2020-12-09 16:37:45 +0530

    In the middle of deployment

--- server/index.js
+++ server/index.js
@@ -1,49 +1,51 @@
-require('dotenv').config({path:process.env.SECRET})
-const authRoute = require('./routes/auth')
-const cors = require('cors')
-const userRoute = require('./routes/user')
-const cateRoute = require('./routes/category')
-const orderRoute = require('./routes/order')
+require("dotenv").config({ path: process.env.SECRET });
+const authRoute = require("./routes/auth");
+const cors = require("cors");
+const userRoute = require("./routes/user");
+const cateRoute = require("./routes/category");
+const orderRoute = require("./routes/order");
+const path = require("path");
+const productRoute = require("./routes/product");
+const paymentRoute = require("./routes/paymentBRoutes");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+mongoose
+  .connect(process.env.DATABASE || process.env.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB succeed");
+  })
+  .catch(() => {
+    console.log("DB stopped working");
+  });
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use("/api", authRoute);
+app.use("/api", userRoute);
+app.use("/api", cateRoute);
+app.use("/api", productRoute);
+app.use("/api", paymentRoute);
+app.use("/api", orderRoute);
 
-const productRoute = require('./routes/product')
-const paymentRoute = require('./routes/paymentBRoutes')
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const app = express()
-mongoose.connect(process.env.DATABASE || process.env.mongoURL,
-                         {useNewUrlParser: true, 
-                            useUnifiedTopology: true,
-                            useCreateIndex: true,
-                            useFindAndModify: false
-                        })
-                        .then( () => {
-                            console.log("DB succeed")
-                            }
-                        )
-                        .catch( () => {
-                                console.log("DB stopped working")
-                            }
-                        )
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors())
-app.use('/api', authRoute)
-app.use('/api', userRoute)
-app.use('/api', cateRoute)
-app.use('/api', productRoute)
-app.use('/api', paymentRoute)
-app.use('/api', orderRoute)
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
 
+const port = 8000 || 3000;
+app.get("/", (req, res) => res.send("hello there"));
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static('client/build'))
-}
-
-const port = 8000 || 3000
-app.get('/', (req, res) => res.send('hello there'))
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
-    console.log(`${port}`)
-})
+  console.log(`${port}`);
+});
